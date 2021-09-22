5ae14a635955417d7f42605f4fd026466901728c
Paige Tiedeman <paigetiedeman@gmail.com>
2021-09-22 10:19:31 -0700

    console interaction set up

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,34 +1,50 @@
-// using System;
-// using System.Collections.Generic;
-// using Palindrome.Models;
+using System;
+using System.Collections.Generic;
+using ToDoList.Models;
 
-// namespace Palindrome
-// {
-//   public class Program
-//   {
-//     public static void Main()
-//     {
-//       PalindromeTest palindromeTest = new PalindromeTest();
-//       Console.WriteLine("Welcome to the Palindrome Checker");
-//       Console.WriteLine("------------");
-//       Console.WriteLine("Please enter a word:");
-//       string word = Console.ReadLine();
-//       if(word == "")
-//       {
-//         Console.WriteLine("Input cannot be blank.");
-//         Main();
-//       }
-//       else
-//       {
-//         if(palindromeTest.IsAPalindrome(word))
-//         {
-//           Console.WriteLine($"{word} is a Palindrome!");
-//         }
-//         else 
-//         {
-//           Console.WriteLine($"{word} is not a Palindrome! Try another");
-//         }
-//       }
-//     }
-//   }
-// }
+namespace ToDoList
+{
+  public class Program
+  {
+    public static void Main()
+    {
+      Console.WriteLine("------------To Do List-------------");
+      Console.WriteLine("Would you like to add an item to your list or view your list?");
+      Console.WriteLine("type add to add to list, type view to view your list");
+      string response = Console.ReadLine();
+      if(response == "add" || response == "Add")
+      {
+        Console.WriteLine("Please Input your new list item");
+        string listItem = Console.ReadLine();
+        Item newItem = new Item(listItem);
+        Console.WriteLine($"{listItem} has been added to your To Do list");
+        Main();
+      }
+      else if (response == "view" || response == "View")
+      {
+          List <Item> outputList = Item.GetAll();
+          int i = 0;
+          foreach (var item in outputList)
+          {
+            ++i;
+            Console.WriteLine($"{i}: {item.Description}");
+          }
+          
+          Main();
+        }
+      else
+      {
+        Console.WriteLine("Please input a valid response, or would you like to exit? Y for try again or [enter] for exit");
+        string userInput = Console.ReadLine();
+        if (userInput == "y" || userInput == "Y")
+        {
+          Main();
+        }
+        else
+        {
+          Console.WriteLine("Goodbye!");
+        }
+      }
+      }
+    }
+}
