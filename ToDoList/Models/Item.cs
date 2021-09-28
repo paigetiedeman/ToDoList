9fad06d1f5970947940b06a2121e2b4c7a21493b
Paige Tiedeman <paigetiedeman@gmail.com>
2021-09-28 08:25:34 -0700

    Project setup using MVC

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -5,27 +5,23 @@ namespace ToDoList.Models
   public class Item
   {
     public string Description { get; set; }
-    public int Priority { get; set; }
-      private static List<Item> _instances = new List<Item> {};
+    private static List<Item> _instances = new List<Item> {};
 
-    public Item(string description)
+    public Item (string description)
     {
       Description = description;
       _instances.Add(this);
     }
 
-    public Item(string description, int priority)
-      : this(description)
-    {
-      Priority = priority;
-    }
     public static List<Item> GetAll()
     {
       return _instances;
     }
+
     public static void ClearAll()
     {
       _instances.Clear();
     }
+
   }
 }
--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,50 +1,20 @@
-using System;
-using System.Collections.Generic;
-using ToDoList.Models;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
 namespace ToDoList
 {
   public class Program
   {
-    public static void Main()
+    public static void Main(string[] args)
     {
-      Console.WriteLine("------------To Do List-------------");
-      Console.WriteLine("Would you like to add an item to your list or view your list?");
-      Console.WriteLine("type add to add to list, type view to view your list");
-      string response = Console.ReadLine();
-      if(response == "add" || response == "Add")
-      {
-        Console.WriteLine("Please Input your new list item");
-        string listItem = Console.ReadLine();
-        Item newItem = new Item(listItem);
-        Console.WriteLine($"{listItem} has been added to your To Do list");
-        Main();
-      }
-      else if (response == "view" || response == "View")
-      {
-          List <Item> outputList = Item.GetAll();
-          int i = 0;
-          foreach (var item in outputList)
-          {
-            ++i;
-            Console.WriteLine($"{i}: {item.Description}");
-          }
-          
-          Main();
-        }
-      else
-      {
-        Console.WriteLine("Please input a valid response, or would you like to exit? Y for try again or [enter] for exit");
-        string userInput = Console.ReadLine();
-        if (userInput == "y" || userInput == "Y")
-        {
-          Main();
-        }
-        else
-        {
-          Console.WriteLine("Goodbye!");
-        }
-      }
-      }
+      var host = new WebHostBuilder()
+        .UseKestrel()
+        .UseContentRoot(Directory.GetCurrentDirectory())
+        .UseIISIntegration()
+        .UseStartup<Startup>()
+        .Build();
+
+      host.Run();
     }
+  }
 }
